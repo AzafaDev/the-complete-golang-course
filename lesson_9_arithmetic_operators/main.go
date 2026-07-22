22101ab33c381e5f061cfc3cef01baebe45877ac
Akmal Dzakwan Faiz <akmal.dz.f@gmail.com>
2026-07-22 21:36:52 +0700

    feat: add lesson 9 demonstrating arithmetic operators in Go

---
+++ lesson_9_arithmetic_operators/main.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 12
+	a %= 5
+	fmt.Println(a)
+	// b := 5
+	// c := 15.50
+
+	// complex1 := complex(10, 15)
+	// complex2 := complex(5, 10)
+
+	// sum := a + b
+	// fmt.Println("sum:", sum)
+
+	// firstName := "code"
+	// lastName := "learn"
+
+	// // fullName := firstName + " " + lastName
+	// // fmt.Println(fullName)
+
+	// fmt.Printf("%s %s\n", firstName, lastName)
+
+	// complexSum := complex1 + complex2
+	// fmt.Println("complexsum:", complexSum)
+
+	// difference := a - b
+	// fmt.Println(difference)
+
+	// product := a * b
+	// fmt.Println("product:", product)
+
+	// division := a / b
+	// fmt.Println("division:", division)
+
+	// reminder := a % b
+	// fmt.Println("reminder", reminder)
+
+	// newSum := a + int(c)
+	// fmt.Println("newsum:", newSum)
+
+}
